7b35191b0b3a3dc57f133df42008cfbc7f1f1ce6
IT19395906 <IT19395906@my.sliit.lk>
2023-11-17 11:38:29 +0530

    initaial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currenttodos) => {
+      return [
+        ...currenttodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currenttodos) => {
+      return currenttodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo;
+      });
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos((currenttodos) => {
+      return currenttodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='new-item-form' onSubmit={handleSubmit}>
+        <div className='form-row'>
+          <label htmlFor='item'>New Item</label>
+          <input type='text' id='item' value={newItem} onChange={(e) => setNewItem(e.target.value)} />
+        </div>
+        <button className='btn'>Add</button>
+      </form>
+      <h1 className='header'>To Do List</h1>
+      <ul className='list'>
+        {todos.length === 0 && "no todo"}
+        {todos.map((todo) => {
+          return <li key={todo.id}>
+            <label>
+              <input type='checkbox' checked={todo.completed} onChange={(e) => toggleTodo(todo.id, e.target.checked)} />
+              {todo.title}
+            </label>
+            <button className='btn btn-danger' onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        })}
+      </ul>
     </div>
   );
 }
